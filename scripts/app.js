7f1ab39f07e447bc1c82cbbc3dd69d279b1d2a32
rohulrez <rezayeerohullah@gmail.com>
2025-04-28 09:46:01 -0700

    DOM logics added for creating& updating & deleting todos vis Vue

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,54 @@
+
 const TodoApp = {
     data() {
         return {
-            newTodo: 'Learn Vue.js'
+            todos: [],
+            enteredTodoText: '',
+            editedTodoId : null
         }
     },
     methods:{
-        saveTodo() {
+        saveTodo(event) {
+            event.preventDefault();
+            if (this.editedTodoId) {
+                const todoId = this.editedTodoId;
+                const todoIndex = this.todos.findIndex(function(todoItem) {
+                    return todoItem.id === todoId;
+                });
 
+                const updatedTodoItem = {
+                    id: this.todos[todoIndex].id,
+                    text: this.enteredTodoText
+                }
+
+                this.todos[todoIndex] = updatedTodoItem;
+                this.editedTodoId =null;
+            } else {
+                const newTodo = {
+                    text: this.enteredTodoText,
+                    id: new Date().toISOString()
+                };
+    
+                this.todos.push(newTodo);
+    
+                this.enteredTodoText = '';
+            }
+            
+        },
+
+        startEditTodo(todoID){
+            this.editedTodoId = todoID;
+            const todo = this.todos.find(function(todoItem) {
+                return todoItem.id === todoID
+            })
+
+            this.enteredTodoText = todo.text;
+        },
+
+        deleteTodo(todoId) {
+            this.todos = this.todos.filter(function(todoItem) {
+                return todoItem.id !== todoId;
+            })
         }
     }
 };
