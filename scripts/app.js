2f777923b19221dbeb1923e98e1b1ceaa70f9769
rohulrez <rezayeerohullah@gmail.com>
2025-04-28 10:40:43 -0700

    Sending http reqs to APIs

--- scripts/app.js
+++ scripts/app.js
@@ -2,16 +2,19 @@
 const TodoApp = {
     data() {
         return {
+            isLoading: false,
             todos: [],
             enteredTodoText: '',
             editedTodoId : null
         }
     },
     methods:{
-        saveTodo(event) {
+        async saveTodo(event) {
             event.preventDefault();
+
             if (this.editedTodoId) {
                 const todoId = this.editedTodoId;
+
                 const todoIndex = this.todos.findIndex(function(todoItem) {
                     return todoItem.id === todoId;
                 });
@@ -23,16 +26,61 @@ const TodoApp = {
 
                 this.todos[todoIndex] = updatedTodoItem;
                 this.editedTodoId =null;
+
+                let response;
+                try {
+                    response = await fetch('http://localhost:3000/todos/' + todoId, {
+                      method: 'PATCH',
+                      body: JSON.stringify({
+                        newText: this.enteredTodoText,
+                      }),
+                      headers: {
+                        'Content-Type': 'application/json',
+                      },
+                    });
+                  } catch (error) {
+                    alert('Something went wrong!');
+                    return;
+                  }
+                
+                  if (!response.ok) {
+                    alert('Something went wrong!');
+                    return;
+                  }
             } else {
-                const newTodo = {
+                let response;
+                try {
+                    response = await fetch('http://localhost:3000/todos', {
+                      method: 'POST',
+                      body: JSON.stringify({
+                        text: this.enteredTodoText,
+                      }),
+                      headers: {
+                        'Content-Type': 'application/json',
+                      },
+                    });
+                  } catch (error) {
+                    alert('Something went wrong!');
+                    return;
+                  }
+
+                  if (!response.ok) {
+                    alert('Something went wrong!');
+                    return;
+                  }
+                
+                  const responseData = await response.json();
+                  const todoId = responseData.createdTodo.id;
+                
+                  const newTodo = {
                     text: this.enteredTodoText,
-                    id: new Date().toISOString()
-                };
+                    id: todoId
+                };                
     
                 this.todos.push(newTodo);
     
-                this.enteredTodoText = '';
             }
+            this.enteredTodoText = '';
             
         },
 
@@ -45,11 +93,49 @@ const TodoApp = {
             this.enteredTodoText = todo.text;
         },
 
-        deleteTodo(todoId) {
+        async deleteTodo(todoId) {
             this.todos = this.todos.filter(function(todoItem) {
-                return todoItem.id !== todoId;
-            })
+            return todoItem.id !== todoId;})
+            
+            
+            let response;
+            try {
+                response = await fetch('http://localhost:3000/todos/' + todoId, {
+                  method: 'DELETE',
+                });
+              } catch (error) {
+                alert('Something went wrong!');
+                return;
+              }
+            
+              if (!response.ok) {
+                alert('Something went wrong!');
+                return;
+              }
+        
         }
+    },
+    async created() {
+        let response;
+        this.isLoading = true;
+        try {
+          response = await fetch('http://localhost:3000/todos');
+        } catch (error) {
+          alert('Something went wrong!');
+          this.isLoading = false;
+          return;
+        }
+
+        this.isLoading = false;
+      
+        if (!response.ok) {
+          alert('Something went wrong!');
+          return;
+        }
+      
+        const responseData = await response.json();
+        this.todos = responseData.todos;
+      
     }
 };
 
